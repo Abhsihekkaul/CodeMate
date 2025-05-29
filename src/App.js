ec8998c40b87793e7bb4f7c380e70479dc120d50
Abhishek Kaul <Abhishekkaul32@gmail.com>
2025-05-29 08:36:11 +0530

    Done Building basic API's with all methods

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@ const express = require("express");
 const ConnectDB = require("./config/database");
 require("dotenv").config();
 const User = require("./Models/user"); 
-
 const app = express();
+
+// Middleware to convert the req.body data from json object to js object so that our server can return this
 app.use(express.json());
 
+
+
+// Sign up user
 app.post('/signup', async (req, res) => {
   try {
     // create a new instance of User (don't overwrite the User model)
-    const newUser = new User({
-      firstName: "Abhishek",
-      lastName: "Kaul",
-      Age: 22,
-      Email: "AbhishekKaul32@gmail.com",
-      Password: "Abhi@2003",
-    });
+    const newUser = new User(req.body);
+
+    console.log(newUser);
 
     await newUser.save();
     res.send("User Created Successfully");
@@ -24,6 +24,55 @@ app.post('/signup', async (req, res) => {
     res.status(500).send("Internal Server Error");
   }
 });
+
+
+// Get User by Email
+app.get("/getUser", async (req, res) => {
+
+  const email = req.body.Email;
+
+  try {
+    const GetUser = await User.find({ Email: email });
+    console.log("User Found:", GetUser);
+    res.send(GetUser);
+  } catch (err) {
+    console.error("Error fetching user:", err.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+
+// Del user api
+app.delete("/delUser", async (req, res) => {
+
+  const UserID = req.body.userID;
+
+  try {
+    const GetUser = await User.findByIdAndDelete(UserID);
+    res.send("User Deleted Successfully!");
+  } catch (err) {
+    console.error("Error fetching user:", err.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// Del user api
+app.patch("/updateUser", async (req, res) => {
+
+  const UserID = req.body.userID;
+  const data = req.body;
+
+  try {
+    const GetUser = await User.findByIdAndUpdate(UserID, data, { returnDocument : 'after'});
+    res.send("User Updated Successfully!");
+  } catch (err) {
+    console.error("Error fetching user:", err.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+
+
 
 // Connect to DB and then start server
 ConnectDB()
